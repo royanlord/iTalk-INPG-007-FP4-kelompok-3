a5ec1afc0f5125b0305718cb01bcd9349fb90bdf
yoohhhaaaannnnnaaaaaa <yohanavanesa7@gmail.com>
2022-11-28 22:59:26 +0700

    manambahkan function konversi datetime ke format 12 jam

--- js/script.js
+++ js/script.js
@@ -15,6 +15,18 @@ var btnSubmit = document.getElementById('btnSubmit');
 var getCity = document.getElementById('inputCity');
 var getDate = document.getElementById('date');
 
+function formatAMPM(date) {
+    date = new Date(date)
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
 btnSubmit.addEventListener('click', function(e) {
     e.preventDefault();
     var setCity = getCity.value;
